7c9b3cf1b9f3401c410c364b047a6f7aa1a8aeb7
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 02:49:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,6 +7,7 @@ require 'directors_database'
 # using director_data as input
 
 def gross_for_director(director_data)
+  
   row_index = 0 
   gross_income = 0  
   column_index = 0
@@ -14,7 +15,7 @@ def gross_for_director(director_data)
           gross_income += director_data[row_index][:movies][column_index][:worldwide_gross]
           column_index += 1 
         end 
-        
+    binding.pry
 end
 
 # Write a method that, given an NDS creates a new Hash
