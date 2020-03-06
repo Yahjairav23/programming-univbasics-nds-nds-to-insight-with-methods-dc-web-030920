a61953788a084ed9d06ccc1709957f43050cfcac
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 02:58:01 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,8 +9,8 @@ require 'directors_database'
 def gross_for_director(director_data)
   gross_income = 0  
   column_index = 0
-  while column_index < director_data[:movies].length do
-          gross_income += director_data[:movies][column_index][:worldwide_gross]
+  while column_index < nds[director_data][:movies].length do
+          gross_income += nds[director_data][:movies][column_index][:worldwide_gross]
           column_index += 1 
         end 
    
