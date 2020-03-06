d3aff9d40e5be5156b7abe40f47fb05a0bbb0bf1
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 02:47:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,7 +24,7 @@ end
 def directors_totals(nds)
       result = {}  
     
-  
+  row_index = 0 
   while row_index < nds.length do
       name_index = 0 
       while name_index < nds[row_index][:name].length do 
